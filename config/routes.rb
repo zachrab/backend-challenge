39709013064c582266f541135997419c98484faf
Zach Rabinovich <zachrab@gmail.com>
2021-05-03 14:17:24 -0700

    Initialize routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Friendships related routes
+  post 'friendships', to: 'friendships#create'
+
+  # Members related routes
+  post 'members', to: 'members#create'
+  get 'members', to: 'members#index'
+  get 'members/:member_id', to: 'members#show'
 end
